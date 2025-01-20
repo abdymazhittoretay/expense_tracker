1a634ec00dfe6c5b40e5b211815db0d3454400ae
Toretay <abdymvzhit@gmail.com>
2025-01-20 22:01:48 +0500

    added expense_model

---
+++ lib/models/expense_model.dart
@@ -0,0 +1,13 @@
+class ExpenseModel {
+  final String name;
+  final double price;
+  final String category;
+  final DateTime date;
+
+  ExpenseModel({
+    required this.name,
+    required this.price,
+    required this.category,
+    required this.date,
+  });
+}
