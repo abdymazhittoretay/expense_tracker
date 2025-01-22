512ac18949b7d8dd1faab9027f2b6aaea608d118
Toretay <abdymvzhit@gmail.com>
2025-01-22 20:56:12 +0500

    created piechart with spending (map) values

--- lib/components/spending_chart.dart
+++ lib/components/spending_chart.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/models/expense_model.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 class SpendingChart extends StatelessWidget {
@@ -15,8 +16,46 @@ class SpendingChart extends StatelessWidget {
           ifAbsent: () => expense.price);
     }
     return Container(
-      padding: EdgeInsets.all(10.0),
+      padding: EdgeInsets.all(16.0),
       height: 360,
+      child: PieChart(
+        PieChartData(
+            sections: spending
+                .map(
+                  (category, totalPrice) => MapEntry(
+                    category,
+                    PieChartSectionData(
+                        color: selectColor(category),
+                        radius: 100.0,
+                        title: "\$${totalPrice.toStringAsFixed(2)}",
+                        titleStyle: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold),
+                        value: totalPrice),
+                  ),
+                )
+                .values
+                .toList(),
+            sectionsSpace: 0),
+      ),
     );
+  }
+
+  // Functions
+  MaterialColor selectColor(String category) {
+    switch (category) {
+      case "Food":
+        return Colors.green;
+      case "Personal":
+        return Colors.purple;
+      case "Entertainment":
+        return Colors.red;
+      case "Trasnportation":
+        return Colors.blue;
+      case "Other":
+        return Colors.grey;
+    }
+    return Colors.grey;
   }
 }
