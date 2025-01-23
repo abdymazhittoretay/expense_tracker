0fb9a7332395396b996e0435ebcce3907c78323b
Toretay <abdymvzhit@gmail.com>
2025-01-23 13:51:11 +0500

    initialized Hive and created boxExpenses

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:expense_tracker/database/boxes.dart';
 import 'package:expense_tracker/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+
+  boxExpenses = await Hive.openBox("expenseBox");
   runApp(const MyApp());
 }
 
